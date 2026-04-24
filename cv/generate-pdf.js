ae44fadffd67dc809b9bfb5e209c5db656d60dfd
William Nauroy <william.nauroy@dice-engineering.com>
2026-04-24 09:26:09 +0200

    Updates

--- cv/generate-pdf.js
+++ cv/generate-pdf.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 (async () => {
   const htmlPath = path.resolve(__dirname, 'index.html');
-  const outputPath = path.resolve(__dirname, '../pdf/cv-william-nauroy.pdf');
+  const outputPath = path.resolve(__dirname, '../pdf/cv-william-nauroy-v2.pdf');
 
   console.log('🚀 Conversion HTML → PDF avec Puppeteer...');
 

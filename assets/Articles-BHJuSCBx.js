dd9878c0a194f09450fe53774a802d0c127f94e1
William Nauroy <william.nauroy@dice-engineering.com>
2026-04-24 16:27:13 +0200

    Updates

--- assets/Articles-BHJuSCBx.js
+++ assets/Articles-BHJuSCBx.js
@@ -1,5 +1,5 @@
-import{a as e,c as t,d as n,i as r,s as i}from"./color-B4VygJcq.js";import{a,t as o}from"./index-DOFxZhe-.js";var s=n(t()),c=e(),l=i.div`
+import{a as e,c as t,d as n,i as r,s as i}from"./color-B4VygJcq.js";import{a,t as o}from"./index-DyIjtO_y.js";var s=n(t()),c=e(),l=i.div`
   padding: 2rem;
   color: ${r};
 `,u=()=>{let[e,t]=(0,s.useState)([]);return(0,s.useEffect)(()=>{(async()=>{t(await o())})()},[]),(0,c.jsxs)(l,{children:[(0,c.jsx)(`h1`,{children:`Article`}),e.map(e=>(0,c.jsxs)(`i`,{children:[`article `,e.title,` `,(0,c.jsx)(a,{to:`/portfolio/article/${e.id}`,children:`voir`})]},e.id))]})};export{u as default};
-//# sourceMappingURL=Articles-UbehJm7y.js.map
+//# sourceMappingURL=Articles-BHJuSCBx.js.map

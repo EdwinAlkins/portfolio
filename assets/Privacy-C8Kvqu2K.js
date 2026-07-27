389dfd8bbf25206b571d9d452427ceafec457845
William Nauroy <william.nauroy@dice-engineering.com>
2026-07-27 22:37:22 +0200

    Updates

--- assets/Privacy-C8Kvqu2K.js
+++ assets/Privacy-C8Kvqu2K.js
@@ -1,4 +1,4 @@
-import"./rolldown-runtime-Bh1tDfsg.js";import{i as e,n as t}from"./analytics-C5fX1eIR.js";import{r as n,t as r}from"./react-vendor-BYMjTRr2.js";import{c as i,l as a,s as o,u as s}from"./index-BP1pvXDa.js";e();var c=r(),l=s.div`
+import"./rolldown-runtime-Bh1tDfsg.js";import{i as e,n as t}from"./analytics-C5fX1eIR.js";import{r as n,t as r}from"./react-vendor-BYMjTRr2.js";import{c as i,l as a,s as o,u as s}from"./index-DybZ15o4.js";e();var c=r(),l=s.div`
   max-width: 720px;
   margin: 0 auto;
   padding: 3rem 1.5rem 5rem;
